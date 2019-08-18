4e46d96d7587c6aa7516d2d3dc8c61780a57cbc7
Raphaël <raphael@digimezzo.com>
2019-08-18 21:30:39 +0200

    Tweaked the welcome screen

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatStepper } from '@angular/material';
 
 @Component({
   selector: 'app-welcome',
@@ -10,7 +11,27 @@ export class WelcomeComponent implements OnInit {
 
   constructor() { }
 
+  public currentStep: number = 0;
+  public totalSteps: number = 6;
+
   ngOnInit() {
   }
 
+  public goBack(stepper: MatStepper): void {
+    if (this.currentStep > 0) {
+      stepper.previous();
+      this.currentStep--;
+    }
+  }
+
+  public goForward(stepper: MatStepper): void {
+    if (this.currentStep < this.totalSteps - 1) {
+      stepper.next();
+      this.currentStep++;
+    }
+  }
+
+  public finish(): void{
+    
+  }
 }
--- src/app/step-indicator/step-indicator.component.ts
+++ src/app/step-indicator/step-indicator.component.ts
@@ -8,13 +8,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class StepIndicatorComponent implements OnInit {
 
   constructor() { 
-    this.totalStepsCollection = Array(5).fill(1).map((x,i)=>i);
   }
+  
   public totalStepsCollection: number[];
 
   @Input() public totalSteps: number;
-  @Input() public activeStep: number;
+  @Input() public currentStep: number;
 
   ngOnInit() {
+    this.totalStepsCollection = Array(this.totalSteps).fill(1).map((x,i)=>i);
   }
 }
