b8bc126731dfa9e8cc0456f5522d4bd873993f6a
Raphaël <raphael@digimezzo.com>
2019-08-23 21:56:48 +0200

    Moved theme switching to AppearanceService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { ElectronService } from './services/electron.service';
 import { TranslateService } from '@ngx-translate/core';
 import { ProductInformation } from './core/productInformation';
-import { remote } from 'electron';
-import { OverlayContainer } from '@angular/cdk/overlay';
 import { Router } from '@angular/router';
-import { Constants } from './core/constants';
 import { Settings } from './core/settings';
 import { Logger } from './core/logger';
+import { AppearanceService } from './services/appearance/appearance.service';
 
 @Component({
   selector: 'app-root',
@@ -15,28 +13,20 @@ import { Logger } from './core/logger';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private globalEmitter = remote.getGlobal('globalEmitter');
-  private themeChangedListener: any = this.applyTheme.bind(this);
-
   constructor(public electronService: ElectronService, private translate: TranslateService, private settings: Settings,
-    private logger: Logger, private overlayContainer: OverlayContainer, public router: Router) {
+    private logger: Logger, private appearance: AppearanceService, public router: Router) {
 
-    this.applyTheme(this.settings.colorTheme, this.settings.useLightBackgroundTheme);
+    this.appearance.applySelectedTheme();
 
     this.translate.setDefaultLang(this.settings.defaultLanguage);
     this.translate.use(this.settings.language);
   }
 
-  public selectedTheme: string;
-
   public ngOnDestroy(): void {
-    this.globalEmitter.remove(Constants.themeChangedEvent, this.themeChangedListener);
   }
 
   public async ngOnInit(): Promise<void> {
     this.logger.info(`+++ Started ${ProductInformation.applicationName} ${ProductInformation.applicationVersion} +++`, "AppComponent", "ngOnInit");
-
-    this.globalEmitter.on(Constants.themeChangedEvent, this.themeChangedListener);
 
     let showWelcome: boolean = this.settings.showWelcome;
 
@@ -46,29 +36,5 @@ export class AppComponent {
     } else {
       this.router.navigate(['/collection']);
     }
-  }
-
-  private applyTheme(themeName: string, useLightBackgroundTheme: boolean): void {
-    let theNameWithBackground: string = `${themeName}-${useLightBackgroundTheme ? "light" : "dark"}`;
-
-    // Apply theme to components in the overlay container: https://gist.github.com/tomastrajan/ee29cd8e180b14ce9bc120e2f7435db7
-    let overlayContainerClasses: DOMTokenList = this.overlayContainer.getContainerElement().classList;
-    let overlayContainerClassesToRemove: string[] = Array.from(overlayContainerClasses).filter((item: string) => item.includes('-theme-'));
-
-    if (overlayContainerClassesToRemove.length) {
-      overlayContainerClasses.remove(...overlayContainerClassesToRemove);
-    }
-
-    overlayContainerClasses.add(theNameWithBackground);
-
-    // Apply theme to body
-    let bodyClasses: DOMTokenList = document.body.classList;
-    let bodyClassesToRemove: string[] = Array.from(bodyClasses).filter((item: string) => item.includes('-theme-'));
-
-    if (bodyClassesToRemove.length) {
-      bodyClasses.remove(...bodyClassesToRemove);
-    }
-
-    document.body.classList.add(theNameWithBackground);
   }
 }
--- src/app/services/appearance/appearance.service.ts
+++ src/app/services/appearance/appearance.service.ts
@@ -1,27 +1,44 @@
 import { Injectable } from '@angular/core';
-import { remote } from 'electron';
 import { Settings } from '../../core/settings';
-import { Constants } from '../../core/constants';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AppearanceService {
-    private globalEmitter = remote.getGlobal('globalEmitter');
-
-    constructor(private settings: Settings) { }
+    constructor(private settings: Settings, private overlayContainer: OverlayContainer) { }
 
     public setColorTheme(colorThemeName: string): void {
         this.settings.colorTheme = colorThemeName;
-
-        // Global event because all windows need to be notified
-        this.globalEmitter.emit(Constants.themeChangedEvent, colorThemeName, this.settings.useLightBackgroundTheme);
+        this.applySelectedTheme();
     }
 
     public setBackgroundTheme(useLightBackgroundTheme: boolean): void {
-      this.settings.useLightBackgroundTheme = useLightBackgroundTheme;
+        this.settings.useLightBackgroundTheme = useLightBackgroundTheme;
+        this.applySelectedTheme();
+    }
 
-      // Global event because all windows need to be notified
-      this.globalEmitter.emit(Constants.themeChangedEvent, this.settings.colorTheme, useLightBackgroundTheme);
-  }
+    public applySelectedTheme(): void {
+        let theNameWithBackground: string = `${this.settings.colorTheme}-${this.settings.useLightBackgroundTheme ? "light" : "dark"}`;
+
+        // Apply theme to components in the overlay container: https://gist.github.com/tomastrajan/ee29cd8e180b14ce9bc120e2f7435db7
+        let overlayContainerClasses: DOMTokenList = this.overlayContainer.getContainerElement().classList;
+        let overlayContainerClassesToRemove: string[] = Array.from(overlayContainerClasses).filter((item: string) => item.includes('-theme-'));
+
+        if (overlayContainerClassesToRemove.length) {
+            overlayContainerClasses.remove(...overlayContainerClassesToRemove);
+        }
+
+        overlayContainerClasses.add(theNameWithBackground);
+
+        // Apply theme to body
+        let bodyClasses: DOMTokenList = document.body.classList;
+        let bodyClassesToRemove: string[] = Array.from(bodyClasses).filter((item: string) => item.includes('-theme-'));
+
+        if (bodyClassesToRemove.length) {
+            bodyClasses.remove(...bodyClassesToRemove);
+        }
+
+        document.body.classList.add(theNameWithBackground);
+    }
 }
