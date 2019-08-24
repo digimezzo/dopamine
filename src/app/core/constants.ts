297a7430458e4caf2dce0ee088289b6fe002b38a
Raphaël <raphael@digimezzo.com>
2019-08-24 09:09:05 +0200

    Added all colors

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -18,6 +18,10 @@ export class Constants {
     ];
 
     static readonly colorThemes: ColorTheme[] = [
-        { name: "default-blue-theme", displayName: "Default blue", color: "#1D7DD4" },
+        { name: "default-blue-theme", displayName: "Blue", color: "#1d7dd4" },
+        { name: "default-green-theme", displayName: "Green", color: "#1db853" },
+        { name: "default-yellow-theme", displayName: "Yellow", color: "#ff9626" },
+        { name: "default-purple-theme", displayName: "Purple", color: "#7a4aba" },
+        { name: "default-pink-theme", displayName: "Pink", color: "#ee205e" },
     ];
 }
