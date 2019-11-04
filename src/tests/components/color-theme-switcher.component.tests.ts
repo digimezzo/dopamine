613ef911a1c98829a28ae7dfebf81c9ce43754ef
Raphaël <raphael@digimezzo.com>
2019-11-04 21:04:09 +0100

    Added unit test for colorThemeSwitcher

---
+++ src/tests/components/color-theme-switcher.component.tests.ts
@@ -0,0 +1,21 @@
+import * as assert from 'assert';
+import * as TypeMoq from "typemoq";
+import { Times } from "typemoq";
+import { ColorThemeSwitcherComponent } from '../../app/components/color-theme-switcher/color-theme-switcher.component';
+import { AppearanceService } from '../../app/services/appearance/appearance.service';
+import { ColorTheme } from '../../app/core/colorTheme';
+
+describe('setColorTheme', () => {
+    it('should change the selected color theme', () => {
+        // Arrange
+        var appearanceServiceMock = TypeMoq.Mock.ofType<AppearanceService>();
+        let colorThemeSwitcher: ColorThemeSwitcherComponent = new ColorThemeSwitcherComponent(appearanceServiceMock.object);
+
+        // Act
+        let blueColorTheme: ColorTheme = new ColorTheme("default-blue-theme", "Blue" , "#1d7dd4");
+        colorThemeSwitcher.setColorTheme(blueColorTheme);
+
+        // Assert
+        appearanceServiceMock.verify(x => x.selectedColorTheme = blueColorTheme, Times.atLeastOnce());
+    });
+});
