a8ba805d7c7e2f21aafb08dd05dc05b2d82edf65
Digimezzo <raphael@digimezzo.com>
2020-06-14 21:22:05 +0200

    Added a unit test for FontSize

---
+++ src/spec/core/font-size.spec.ts
@@ -0,0 +1,33 @@
+import * as assert from 'assert';
+import { FontSize } from '../../app/core/font-size';
+
+describe('FontSize', () => {
+    describe('constructor', () => {
+        it('Should have normal size', () => {
+            // Arrange
+            // Act
+            const fontSize = new FontSize(14);
+
+            // Assert
+            assert.ok(fontSize.normalSize === 14);
+        });
+
+        it('Should have larger size', () => {
+            // Arrange
+            // Act
+            const fontSize = new FontSize(14);
+
+            // Assert
+            assert.ok(fontSize.largerSize === 14 * 1.42);
+        });
+
+        it('Should have largest size', () => {
+            // Arrange
+            // Act
+            const fontSize = new FontSize(14);
+
+            // Assert
+            assert.ok(fontSize.largestSize === 14 * 2.85);
+        });
+    });
+});
