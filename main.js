a8fc824cde80602133bd794b3c8b292e111691dc
Digimezzo <raphael@digimezzo.com>
2020-06-27 10:29:05 +0200

    Adding of folders is safer

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ function createWindow() {
         }));
     }
     if (serve) {
-        win.webContents.openDevTools();
+        // win.webContents.openDevTools();
     }
     // Emitted when the window is closed.
     win.on('closed', function () {
--- src/app/services/folder/folder.service.ts
+++ src/app/services/folder/folder.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { FolderServiceBase } from './folder-service-base';
 import { FolderRepository } from '../../data/entities/folder-repository';
+import { Logger } from '../../core/logger';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FolderService implements FolderServiceBase {
-  constructor(private folderRepository: FolderRepository) { }
+  constructor(private folderRepository: FolderRepository, private logger: Logger) { }
 
   public async addFolderAsync(path: string): Promise<void> {
-    await this.folderRepository.addFolderAsync(path);
+    try {
+      await this.folderRepository.addFolderAsync(path);
+      this.logger.info(`Added folder with path '${path}'`, 'FolderService', 'addFolderAsync');
+    } catch (error) {
+      this.logger.info(`Could not add folder with path '${path}'. Error: ${error}`, 'FolderService', 'addFolderAsync');
+    }
   }
 }
