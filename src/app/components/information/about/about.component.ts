998e96444307b64261542f89fae96489891b6e12
Digimezzo Raphaël <raphael@digimezzo.com>
2020-10-28 12:55:35 +0100

    Added items to about screen

--- src/app/components/information/about/about.component.ts
+++ src/app/components/information/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ContactInformation } from '../../../core/base/contact-information';
+import { ProductInformation } from '../../../core/base/product-information';
 
 @Component({
   selector: 'app-about',
@@ -10,6 +12,12 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
+
+  public applicationVersion: string = ProductInformation.applicationVersion;
+  public applicationCopyright: string = ProductInformation.applicationCopyright;
+  public websiteUrl: string = ContactInformation.websiteUrl;
+  public twitterUrl: string = ContactInformation.twitterUrl;
+  public githubUrl: string = ContactInformation.githubUrl;
 
   public ngOnInit(): void {
   }
