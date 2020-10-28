59fb22bc7a76cee5d738565b7131641f36e1207b
Digimezzo Raphaël <raphael@digimezzo.com>
2020-10-28 11:49:43 +0100

    Added unit test for components screen, added recommended vscode settings

--- src/app/components/information/components/components.component.ts
+++ src/app/components/information/components/components.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Constants } from '../../../core/base/constants';
+import { ExternalComponent } from '../../../core/base/external-component';
 
 @Component({
   selector: 'app-components',
@@ -12,7 +13,7 @@ export class ComponentsComponent implements OnInit {
 
   constructor() { }
 
-  public externalComponents: any[] = Constants.externalComponents;
+  public externalComponents: ExternalComponent[] = Constants.externalComponents;
 
   public ngOnInit(): void {
   }
