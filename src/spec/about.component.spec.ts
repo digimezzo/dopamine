b9109a129766e781f8ed68a240e962912ae6273a
Digimezzo Raphaël <raphael@digimezzo.com>
2020-10-28 14:21:08 +0100

    Adde unit test for license dialog on about screen

--- src/spec/about.component.spec.ts
+++ src/spec/about.component.spec.ts
@@ -1,13 +1,16 @@
 import * as assert from 'assert';
+import { IMock, Mock, Times } from 'typemoq';
 import { AboutComponent } from '../app/components/information/about/about.component';
 import { ContactInformation } from '../app/core/base/contact-information';
 import { ProductInformation } from '../app/core/base/product-information';
+import { BaseDialogService } from '../app/services/dialog/base-dialog.service';
 
 describe('AboutComponent', () => {
     describe('constructor', () => {
         it('Should set application version', async () => {
             // Arrange
-            const aboutComponent: AboutComponent = new AboutComponent();
+            const dialogServiceMock: IMock<BaseDialogService> = Mock.ofType<BaseDialogService>();
+            const aboutComponent: AboutComponent = new AboutComponent(dialogServiceMock.object);
 
             // Act
             const applicationVersion: string = await aboutComponent.applicationVersion;
@@ -18,7 +21,8 @@ describe('AboutComponent', () => {
 
         it('Should set application Copyright', async () => {
             // Arrange
-            const aboutComponent: AboutComponent = new AboutComponent();
+            const dialogServiceMock: IMock<BaseDialogService> = Mock.ofType<BaseDialogService>();
+            const aboutComponent: AboutComponent = new AboutComponent(dialogServiceMock.object);
 
             // Act
             const applicationCopyright: string = await aboutComponent.applicationCopyright;
@@ -29,7 +33,8 @@ describe('AboutComponent', () => {
 
         it('Should set website URL', async () => {
             // Arrange
-            const aboutComponent: AboutComponent = new AboutComponent();
+            const dialogServiceMock: IMock<BaseDialogService> = Mock.ofType<BaseDialogService>();
+            const aboutComponent: AboutComponent = new AboutComponent(dialogServiceMock.object);
 
             // Act
             const websiteUrl: string = await aboutComponent.websiteUrl;
@@ -40,7 +45,8 @@ describe('AboutComponent', () => {
 
         it('Should set Twitter URL', async () => {
             // Arrange
-            const aboutComponent: AboutComponent = new AboutComponent();
+            const dialogServiceMock: IMock<BaseDialogService> = Mock.ofType<BaseDialogService>();
+            const aboutComponent: AboutComponent = new AboutComponent(dialogServiceMock.object);
 
             // Act
             const twitterUrl: string = await aboutComponent.twitterUrl;
@@ -51,13 +57,27 @@ describe('AboutComponent', () => {
 
         it('Should set GitHub URL', async () => {
             // Arrange
-            const aboutComponent: AboutComponent = new AboutComponent();
+            const dialogServiceMock: IMock<BaseDialogService> = Mock.ofType<BaseDialogService>();
+            const aboutComponent: AboutComponent = new AboutComponent(dialogServiceMock.object);
 
             // Act
             const githubUrl: string = await aboutComponent.githubUrl;
 
             // Assert
             assert.strictEqual(githubUrl, ContactInformation.githubUrl);
+        });
+    });
+    describe('showLicenseDialog', () => {
+        it('Should open a license dialog', () => {
+            // Arrange
+            const dialogServiceMock: IMock<BaseDialogService> = Mock.ofType<BaseDialogService>();
+            const aboutComponent: AboutComponent = new AboutComponent(dialogServiceMock.object);
+
+            // Act
+            aboutComponent.showLicenseDialog();
+
+            // Assert
+            dialogServiceMock.verify(x => x.showLicenseDialog(), Times.exactly(1));
         });
     });
 });
