dd5422b39d01af1e4a611b60b6a89302e447e86f
Digimezzo Raphaël <raphael@digimezzo.com>
2020-11-07 18:59:25 +0100

    StatusMessage now has a unique id

--- src/app/services/status/status-message.ts
+++ src/app/services/status/status-message.ts
@@ -1,4 +1,9 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class StatusMessage {
     constructor(public message: string, public isDismissable: boolean) {
+        this.id = uuidv4();
     }
+
+    public readonly id: string;
 }
