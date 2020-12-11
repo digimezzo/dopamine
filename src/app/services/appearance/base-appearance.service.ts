5ea5c8e466cfbcce82ac16bc0f37a022b2a528fa
Digimezzo Raphaël <raphael@digimezzo.com>
2020-12-11 08:59:39 +0100

    Fixed some mistakes

--- src/app/services/appearance/base-appearance.service.ts
+++ src/app/services/appearance/base-appearance.service.ts
@@ -1,12 +1,15 @@
+import { FontSize } from '../../core/base/font-size';
 import { ColorScheme } from './color-scheme';
 
 export abstract class BaseAppearanceService {
     public abstract get windowHasNativeTitleBar(): boolean;
+    public abstract fontSizes: FontSize[];
     public abstract colorSchemes: ColorScheme[];
     public abstract followSystemTheme: boolean;
     public abstract useLightBackgroundTheme: boolean;
     public abstract followSystemColor: boolean;
     public abstract selectedColorScheme: ColorScheme;
+    public abstract selectedFontSize: FontSize;
     public abstract applyTheme(): void;
     public abstract applyFontSize(): void;
 }
