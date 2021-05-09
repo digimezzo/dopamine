a9d1342d182702c15e2b96c54974a91280a89bf8
Digimezzo Raphaël <raphael@digimezzo.com>
2021-05-09 16:25:15 +0200

    Building TrackBrowser

--- src/app/components/collection/track-browser/track-browser.component.ts
+++ src/app/components/collection/track-browser/track-browser.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TrackModel } from '../../../services/track/track-model';
 
 @Component({
     selector: 'app-track-browser',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./track-browser.component.scss'],
 })
 export class TrackBrowserComponent implements OnInit {
+    private _tracks: TrackModel[] = [];
+
     constructor() {}
+
+    public get tracks(): TrackModel[] {
+        return this._tracks;
+    }
+
+    @Input()
+    public set albums(v: TrackModel[]) {
+        this._tracks = v;
+    }
 
     public ngOnInit(): void {}
 }
