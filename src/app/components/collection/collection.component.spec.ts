bdc16d2dd77dc08c0c08d2ada91801996adff2d7
Digimezzo Raphaël <raphael@digimezzo.com>
2021-05-21 07:45:23 +0200

    Added CollectionComponent tests

--- src/app/components/collection/collection.component.spec.ts
+++ src/app/components/collection/collection.component.spec.ts
@@ -1,216 +1,65 @@
-// import { IMock, Mock } from 'typemoq';
-// import { BaseSettings } from '../../core/settings/base-settings';
-// import { BaseAppearanceService } from '../../services/appearance/base-appearance.service';
-// import { CollectionComponent } from './collection.component';
+import { IMock, Mock, Times } from 'typemoq';
+import { BaseAppearanceService } from '../../services/appearance/base-appearance.service';
+import { CollectionPersister } from './collection-persister';
+import { CollectionComponent } from './collection.component';
 
-// describe('CollectionComponent', () => {
-//     let appearanceServiceMock: IMock<BaseAppearanceService>;
-//     let settingsMock: IMock<BaseSettings>;
+describe('CollectionComponent', () => {
+    let appearanceServiceMock: IMock<BaseAppearanceService>;
+    let collectionPersisterMock: IMock<CollectionPersister>;
 
-//     let component: CollectionComponent;
+    let component: CollectionComponent;
 
-//     beforeEach(() => {
-//         appearanceServiceMock = Mock.ofType<BaseAppearanceService>();
-//         settingsMock = Mock.ofType<BaseSettings>();
-//         component = new CollectionComponent(appearanceServiceMock.object, settingsMock.object);
-//     });
+    beforeEach(() => {
+        appearanceServiceMock = Mock.ofType<BaseAppearanceService>();
+        collectionPersisterMock = Mock.ofType<CollectionPersister>();
 
-//     describe('constructor', () => {
-//         it('should create', () => {
-//             // Arrange
+        collectionPersisterMock.setup((x) => x.getSelectedTabIndex()).returns(() => 3);
 
-//             // Act
+        component = new CollectionComponent(appearanceServiceMock.object, collectionPersisterMock.object);
+    });
 
-//             // Assert
-//             expect(component).toBeDefined();
-//         });
+    describe('constructor', () => {
+        it('should create', () => {
+            // Arrange
 
-//         it('should define appearanceService', async () => {
-//             // Arrange
+            // Act
 
-//             // Act
-//             await component.ngOnInit();
+            // Assert
+            expect(component).toBeDefined();
+        });
 
-//             // Assert
-//             expect(component.appearanceService).toBeDefined();
-//         });
-//     });
+        it('should define appearanceService', async () => {
+            // Arrange
 
-//     describe('ngOnInit', () => {
-//         it('should set the artists tab from the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: 'artists' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
+            // Act
+            await component.ngOnInit();
 
-//             // Act
-//             await component.ngOnInit();
+            // Assert
+            expect(component.appearanceService).toBeDefined();
+        });
+    });
 
-//             // Assert
-//             expect(component.selectedIndex).toEqual(0);
-//         });
+    describe('ngOnInit', () => {
+        it('should set the selected index from the settings', async () => {
+            // Arrange
 
-//         it('should set the genres tab from the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: 'genres' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
+            // Act
+            await component.ngOnInit();
 
-//             // Act
-//             await component.ngOnInit();
+            // Assert
+            expect(component.selectedIndex).toEqual(3);
+        });
+    });
 
-//             // Assert
-//             expect(component.selectedIndex).toEqual(1);
-//         });
+    describe('selectedIndex', () => {
+        it('should save the select tab index to the settings', () => {
+            // Arrange
 
-//         it('should set the albums tab from the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: 'albums' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
+            // Act
+            component.selectedIndex = 2;
 
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(component.selectedIndex).toEqual(2);
-//         });
-
-//         it('should set the songs tab from the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: 'tracks' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(component.selectedIndex).toEqual(3);
-//         });
-
-//         it('should set the playlists tab from the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: 'playlists' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(component.selectedIndex).toEqual(4);
-//         });
-
-//         it('should set the folders tab from the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: 'folders' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(component.selectedIndex).toEqual(5);
-//         });
-
-//         it('should set the artists tab if the settings contain an unknown tab', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: 'unknown' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(component.selectedIndex).toEqual(0);
-//         });
-
-//         it('should set the artists tab if the settings contain an empty tab', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: '' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(component.selectedIndex).toEqual(0);
-//         });
-//     });
-
-//     describe('selectedIndex', () => {
-//         it('should save the artists tab to the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: '' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-//             component.selectedIndex = 0;
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(settingsStub.selectedTab).toEqual('artists');
-//         });
-
-//         it('should save the genres tab to the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: '' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-//             component.selectedIndex = 1;
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(settingsStub.selectedTab).toEqual('genres');
-//         });
-
-//         it('should save the albums tab to the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: '' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-//             component.selectedIndex = 2;
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(settingsStub.selectedTab).toEqual('albums');
-//         });
-
-//         it('should save the tracks tab to the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: '' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-//             component.selectedIndex = 3;
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(settingsStub.selectedTab).toEqual('tracks');
-//         });
-
-//         it('should save the playlists tab to the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: '' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-//             component.selectedIndex = 4;
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(settingsStub.selectedTab).toEqual('playlists');
-//         });
-
-//         it('should save the folders tab to the settings', async () => {
-//             // Arrange
-//             const settingsStub: any = { selectedTab: '' };
-//             component = new CollectionComponent(appearanceServiceMock.object, settingsStub);
-//             component.selectedIndex = 5;
-
-//             // Act
-//             await component.ngOnInit();
-
-//             // Assert
-//             expect(settingsStub.selectedTab).toEqual('folders');
-//         });
-//     });
-// });
+            // Assert
+            collectionPersisterMock.verify((x) => x.setSelectedTabFromTabIndex(2), Times.exactly(1));
+        });
+    });
+});
