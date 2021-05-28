6a818a7502689c8d9b950c86ecec259f3a74c10d
Digimezzo Raphaël <raphael@digimezzo.com>
2021-05-28 19:11:24 +0200

    VersionComparer can now compare including pre-releases

--- src/app/services/update/version-comparer.spec.ts
+++ src/app/services/update/version-comparer.spec.ts
@@ -31,5 +31,155 @@ describe('VersionComparer', () => {
             // Assert
             expect(isNewerVersion).toBeFalsy();
         });
+
+        it('should not report newer version if old version is the same as new version but new version has "next" suffix', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0', '3.0.0-next.1');
+
+            // Assert
+            expect(isNewerVersion).toBeFalsy();
+        });
+
+        it('should not report newer version if old version is the same as new version but new version has "rc" suffix', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0', '3.0.0-rc.1');
+
+            // Assert
+            expect(isNewerVersion).toBeFalsy();
+        });
+
+        it('should not report newer version if old version is newer than new version and new version has "next" suffix', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.1', '3.0.1-next.1');
+
+            // Assert
+            expect(isNewerVersion).toBeFalsy();
+        });
+
+        it('should not report newer version if old version is newer than new version and new version has "rc" suffix', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.1', '3.0.0-rc.1');
+
+            // Assert
+            expect(isNewerVersion).toBeFalsy();
+        });
+
+        it('should report newer version if old version is older than new version and new version has "next" suffix', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0', '3.0.1-next.1');
+
+            // Assert
+            expect(isNewerVersion).toBeTruthy();
+        });
+
+        it('should report newer version if old version is older than new version and new version has "rc" suffix', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0', '3.0.1-rc.1');
+
+            // Assert
+            expect(isNewerVersion).toBeTruthy();
+        });
+
+        it('should report newer version if both old and new version are "next" versions with same version number but new version has higher iteration', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0-next.1', '3.0.0-next.2');
+
+            // Assert
+            expect(isNewerVersion).toBeTruthy();
+        });
+
+        it('should report newer version if both old and new version are "rc" versions with same version number but new version has higher iteration', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0-rc.1', '3.0.0-rc.2');
+
+            // Assert
+            expect(isNewerVersion).toBeTruthy();
+        });
+
+        it('should report not newer version if both old and new version are "next" versions with same version number but new version has lower iteration', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0-next.2', '3.0.0-next.1');
+
+            // Assert
+            expect(isNewerVersion).toBeFalsy();
+        });
+
+        it('should report not newer version if both old and new version are "rc" versions with same version number but new version has lower iteration', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0-rc.2', '3.0.0-rc.1');
+
+            // Assert
+            expect(isNewerVersion).toBeFalsy();
+        });
+
+        it('should report newer version if old version is "next" version and new version is "rc" version and both have the same version number and iteration', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0-next.1', '3.0.0-rc.1');
+
+            // Assert
+            expect(isNewerVersion).toBeTruthy();
+        });
+
+        it('should report not newer version if old version is "rc" version and new version is "next" version and both have the same version number and iteration', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0-rc.1', '3.0.0-next.1');
+
+            // Assert
+            expect(isNewerVersion).toBeFalsy();
+        });
+
+        it('should report not newer version if old version is "rc" version and new version is "next" version and iteration of new version is higher', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0-rc.1', '3.0.0-next.2');
+
+            // Assert
+            expect(isNewerVersion).toBeFalsy();
+        });
+
+        it('should report not newer version if old version is "rc" version and new version is "next" version and iteration of new version is lower', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0-rc.5', '3.0.0-next.1');
+
+            // Assert
+            expect(isNewerVersion).toBeFalsy();
+        });
+
+        it('should report newer version if old version is "next" version and new version is "rc" version and iteration of new version is lower', async () => {
+            // Arrange
+
+            // Act
+            const isNewerVersion: boolean = VersionComparer.isNewerVersion('3.0.0-next.6', '3.0.0-rc.1');
+
+            // Assert
+            expect(isNewerVersion).toBeTruthy();
+        });
     });
 });
--- src/app/services/update/version-comparer.ts
+++ src/app/services/update/version-comparer.ts
@@ -1,27 +1,73 @@
 export class VersionComparer {
     public static isNewerVersion(oldVersion: string, newVersion: string): boolean {
-        const oldVersionParts = oldVersion.split('.');
-        const newVersionParts = newVersion.split('.');
+        const oldVersionParts: string[] = oldVersion.split('-');
+        const newVersionParts: string[] = newVersion.split('-');
+
+        if (oldVersionParts.length === 1 && newVersionParts.length === 1) {
+            return this.isNewerVersionNumber(oldVersionParts[0], newVersionParts[0]);
+        }
+
+        if (oldVersionParts.length === 2 && newVersionParts.length === 2) {
+            if (oldVersionParts[0] === newVersionParts[0]) {
+                return this.isNewerVersionTag(oldVersionParts[1], newVersionParts[1]);
+            }
+
+            return (
+                this.isNewerVersionNumber(oldVersionParts[0], newVersionParts[0]) &&
+                this.isNewerVersionTag(oldVersionParts[1], newVersionParts[1])
+            );
+        }
+
+        if (oldVersionParts.length === 1 && newVersionParts.length === 2) {
+            return this.isNewerVersionNumber(oldVersionParts[0], newVersionParts[0]);
+        }
+
+        return false;
+    }
+
+    private static isNewerVersionNumber(oldVersionNumberAsString: string, newVersionNumberAsString: string): boolean {
+        const oldVersionParts = oldVersionNumberAsString.split('.');
+        const newVersionParts = newVersionNumberAsString.split('.');
 
         for (let i = 0; i < newVersionParts.length; i++) {
             let newVersionNumber: number = parseInt(newVersionParts[i], 10);
             let oldVersionNumber: number = parseInt(oldVersionParts[i], 10);
-
             if (newVersionNumber == undefined || Number.isNaN(newVersionNumber)) {
                 newVersionNumber = 0;
             }
-
             if (oldVersionNumber == undefined || Number.isNaN(oldVersionNumber)) {
                 oldVersionNumber = 0;
             }
-
             if (newVersionNumber > oldVersionNumber) {
                 return true;
             }
-
             if (newVersionNumber < oldVersionNumber) {
                 return false;
             }
+        }
+        return false;
+    }
+
+    private static isNewerVersionTag(oldVersionTag: string, newVersionTag: string): boolean {
+        if (oldVersionTag.includes('rc') && newVersionTag.includes('next')) {
+            return false;
+        }
+
+        if (oldVersionTag.includes('next') && newVersionTag.includes('rc')) {
+            return true;
+        }
+
+        if (
+            (oldVersionTag.includes('next') && newVersionTag.includes('next')) ||
+            (oldVersionTag.includes('rc') && newVersionTag.includes('rc'))
+        ) {
+            const oldVersionTagParts: string[] = oldVersionTag.split('.');
+            const newVersionTagParts: string[] = newVersionTag.split('.');
+
+            const oldVersionIteration: number = parseInt(oldVersionTagParts[1], 10);
+            const newVersionIteration: number = parseInt(newVersionTagParts[1], 10);
+
+            return newVersionIteration > oldVersionIteration;
         }
 
         return false;
