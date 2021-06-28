a2ca08a7d9ba1b1156fbc43db45f293adac68eac
Digimezzo Raphaël <raphael@digimezzo.com>
2021-06-28 12:12:10 +0200

    Re-added album artwork notification

--- src/app/services/indexing/album-artwork-indexer.spec.ts
+++ src/app/services/indexing/album-artwork-indexer.spec.ts
@@ -76,6 +76,17 @@ describe('AlbumArtworkIndexer', () => {
             albumArtworkRemoverMock.verify((x) => x.removeAlbumArtworkThatIsNotInTheDatabaseFromDiskAsync(), Times.exactly(1));
         });
 
+        it('should indicate that album artwork is being indexed', async () => {
+            // Arrange
+            trackRepositoryMock.setup((x) => x.getAlbumDataThatNeedsIndexing()).returns(() => []);
+
+            // Act
+            await albumArtworkIndexer.indexAlbumArtworkAsync();
+
+            // Assert
+            snackBarServiceMock.verify((x) => x.updatingAlbumArtworkAsync(), Times.exactly(1));
+        });
+
         it('should dismiss the indexing notification with a short delay', async () => {
             // Arrange
             trackRepositoryMock.setup((x) => x.getAlbumDataThatNeedsIndexing()).returns(() => []);
