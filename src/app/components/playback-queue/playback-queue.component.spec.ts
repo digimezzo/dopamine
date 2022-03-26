d356e67f95e3391b6731c5ad88fec510dca020aa
Digimezzo Raphaël <raphael@digimezzo.com>
2022-03-26 19:16:20 +0100

    Preparations to remove tracks from queue

--- src/app/components/playback-queue/playback-queue.component.spec.ts
+++ src/app/components/playback-queue/playback-queue.component.spec.ts
@@ -1,6 +1,9 @@
 import { Observable, Subject } from 'rxjs';
 import { IMock, Mock, Times } from 'typemoq';
+import { ContextMenuOpener } from '../../common/context-menu-opener';
 import { Track } from '../../common/data/entities/track';
+import { MouseSelectionWatcher } from '../../common/mouse-selection-watcher';
+import { BaseNavigationService } from '../../services/navigation/base-navigation.service';
 import { BasePlaybackIndicationService } from '../../services/playback-indication/base-playback-indication.service';
 import { BasePlaybackService } from '../../services/playback/base-playback.service';
 import { PlaybackStarted } from '../../services/playback/playback-started';
@@ -9,17 +12,33 @@ import { TrackModels } from '../../services/track/track-models';
 import { BaseTranslatorService } from '../../services/translator/base-translator.service';
 import { PlaybackQueueComponent } from './playback-queue.component';
 
-describe('PlaybackInformationComponent', () => {
-    let component: PlaybackQueueComponent;
+describe('PlaybackQueueComponent', () => {
     let playbackServiceMock: IMock<BasePlaybackService>;
+    let contextMenuOpenerMock: IMock<ContextMenuOpener>;
+    let mouseSelectionWatcherMock: IMock<MouseSelectionWatcher>;
     let playbackIndicationServiceMock: IMock<BasePlaybackIndicationService>;
+    let navigationServiceMock: IMock<BaseNavigationService>;
     let translatorServiceMock: IMock<BaseTranslatorService>;
     let playbackQueue: TrackModels;
     let playbackServicePlaybackStarted: Subject<PlaybackStarted>;
+    let navigationServiceShowPlaybackQueueRequested: Subject<void>;
+
+    function createComponent(): PlaybackQueueComponent {
+        return new PlaybackQueueComponent(
+            playbackServiceMock.object,
+            contextMenuOpenerMock.object,
+            mouseSelectionWatcherMock.object,
+            playbackIndicationServiceMock.object,
+            navigationServiceMock.object
+        );
+    }
 
     beforeEach(async () => {
         playbackServiceMock = Mock.ofType<BasePlaybackService>();
+        contextMenuOpenerMock = Mock.ofType<ContextMenuOpener>();
+        mouseSelectionWatcherMock = Mock.ofType<MouseSelectionWatcher>();
         playbackIndicationServiceMock = Mock.ofType<BasePlaybackIndicationService>();
+        navigationServiceMock = Mock.ofType<BaseNavigationService>();
         translatorServiceMock = Mock.ofType<BaseTranslatorService>();
 
         playbackServicePlaybackStarted = new Subject();
@@ -31,7 +50,9 @@ describe('PlaybackInformationComponent', () => {
 
         playbackServiceMock.setup((x) => x.playbackQueue).returns(() => playbackQueue);
 
-        component = new PlaybackQueueComponent(playbackServiceMock.object, playbackIndicationServiceMock.object);
+        navigationServiceShowPlaybackQueueRequested = new Subject();
+        const navigationServiceShowPlaybackQueueRequested$: Observable<void> = navigationServiceShowPlaybackQueueRequested.asObservable();
+        navigationServiceMock.setup((x) => x.showPlaybackQueueRequested$).returns(() => navigationServiceShowPlaybackQueueRequested$);
     });
 
     describe('constructor', () => {
@@ -39,6 +60,7 @@ describe('PlaybackInformationComponent', () => {
             // Arrange
 
             // Act
+            const component: PlaybackQueueComponent = createComponent();
 
             // Assert
             expect(component).toBeDefined();
@@ -48,9 +70,40 @@ describe('PlaybackInformationComponent', () => {
             // Arrange
 
             // Act
+            const component: PlaybackQueueComponent = createComponent();
 
             // Assert
             expect(component.playbackService).toBeDefined();
+        });
+
+        it('should define contextMenuOpener', () => {
+            // Arrange
+
+            // Act
+            const component: PlaybackQueueComponent = createComponent();
+
+            // Assert
+            expect(component.contextMenuOpener).toBeDefined();
+        });
+
+        it('should declare trackContextMenu', () => {
+            // Arrange
+
+            // Act
+            const component: PlaybackQueueComponent = createComponent();
+
+            // Assert
+            expect(component.trackContextMenu).toBeUndefined();
+        });
+
+        it('should define mouseSelectionWatcher', () => {
+            // Arrange
+
+            // Act
+            const component: PlaybackQueueComponent = createComponent();
+
+            // Assert
+            expect(component.mouseSelectionWatcher).toBeDefined();
         });
     });
 
@@ -59,13 +112,56 @@ describe('PlaybackInformationComponent', () => {
             // Arrange
             const trackModel: TrackModel = new TrackModel(new Track('DummyPath'), translatorServiceMock.object);
             const playbackStarted: PlaybackStarted = new PlaybackStarted(trackModel, false);
+            const component: PlaybackQueueComponent = createComponent();
 
             // Act
             component.ngOnInit();
             playbackServicePlaybackStarted.next(playbackStarted);
 
             // Assert
-            playbackIndicationServiceMock.verify((x) => x.setPlayingTrack(playbackQueue.tracks, trackModel), Times.exactly(1));
+            playbackIndicationServiceMock.verify((x) => x.setPlayingTrack(playbackQueue.tracks, trackModel), Times.once());
+        });
+
+        it('should initialize mouseSelectionWatcher using the tracks in the queue when the playback queue is shown', () => {
+            // Arrange
+            const component: PlaybackQueueComponent = createComponent();
+
+            // Act
+            component.ngOnInit();
+            navigationServiceShowPlaybackQueueRequested.next();
+
+            // Assert
+            mouseSelectionWatcherMock.verify((x) => x.initialize(playbackQueue.tracks), Times.once());
+        });
+    });
+
+    describe('onTrackContextMenu', () => {
+        it('should open the track context menu', () => {
+            // Arrange
+            const trackModel: TrackModel = new TrackModel(new Track('DummyPath'), translatorServiceMock.object);
+            const component: PlaybackQueueComponent = createComponent();
+            const event: any = {};
+
+            // Act
+            component.onTrackContextMenu(event, trackModel);
+
+            // Assert
+            contextMenuOpenerMock.verify((x) => x.open(component.trackContextMenu, event, trackModel), Times.once());
+        });
+    });
+
+    describe('setSelectedTracks', () => {
+        it('should set the selected item on mouseSelectionWatcher', () => {
+            // Arrange
+            const component: PlaybackQueueComponent = createComponent();
+            const event: any = {};
+            const trackModel: TrackModel = new TrackModel(new Track('Path 1'), translatorServiceMock.object);
+
+            // Act
+            component.setSelectedTracks(event, trackModel);
+
+            // Assert
+            mouseSelectionWatcherMock.verify((x) => x.setSelectedItems(event, trackModel), Times.once());
         });
     });
 });
