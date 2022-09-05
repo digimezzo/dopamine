66f481491791736dd0e5178ee8c23af768b2910f
Digimezzo Raphaël <raphael@digimezzo.com>
2022-09-05 04:28:18 +0200

    Fixed missing node polyfills

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+    resolve: {
+        alias: {
+            constants: require.resolve('constants-browserify'),
+            crypto: require.resolve('crypto-browserify'),
+            fs: require.resolve('browserify-fs'),
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            os: require.resolve('os-browserify'),
+            path: require.resolve('path-browserify'),
+            readline: require.resolve('readline-browserify'),
+            stream: require.resolve('stream-browserify'),
+            timers: require.resolve('timers-browserify'),
+        },
+    },
     externals: {
         'better-sqlite3': 'commonjs better-sqlite3',
     },
