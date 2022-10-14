03dea947cb113ec4a749d51447717f9e065aaeb2
Digimezzo Raphaël <raphael@digimezzo.com>
2022-10-14 11:40:23 +0200

    Improved display of total durations

--- src/app/pipes/format-total-duration.pipe.spec.ts
+++ src/app/pipes/format-total-duration.pipe.spec.ts
@@ -8,9 +8,13 @@ describe('FormatTotalDurationPipe', () => {
 
     beforeEach(() => {
         translatorServiceMock = Mock.ofType<BaseTranslatorService>();
+        translatorServiceMock.setup((x) => x.get('day')).returns(() => 'day');
         translatorServiceMock.setup((x) => x.get('days')).returns(() => 'days');
+        translatorServiceMock.setup((x) => x.get('hour')).returns(() => 'hour');
         translatorServiceMock.setup((x) => x.get('hours')).returns(() => 'hours');
+        translatorServiceMock.setup((x) => x.get('minute')).returns(() => 'minute');
         translatorServiceMock.setup((x) => x.get('minutes')).returns(() => 'minutes');
+        translatorServiceMock.setup((x) => x.get('second')).returns(() => 'second');
         translatorServiceMock.setup((x) => x.get('seconds')).returns(() => 'seconds');
         formatTotalDurationPipe = new FormatTotalDurationPipe(translatorServiceMock.object);
     });
@@ -46,14 +50,14 @@ describe('FormatTotalDurationPipe', () => {
             expect(formattedTotalDuration).toEqual('');
         });
 
-        it('should return "1 seconds" if total duration is 1000 milliseconds', () => {
+        it('should return "1 second" if total duration is 1000 milliseconds', () => {
             // Arrange
 
             // Act
             const formattedTotalDuration: string = formatTotalDurationPipe.transform(1000);
 
             // Assert
-            expect(formattedTotalDuration).toEqual('1 seconds');
+            expect(formattedTotalDuration).toEqual('1 second');
         });
 
         it('should return "59 seconds" if total duration is 59000 milliseconds', () => {
@@ -66,14 +70,14 @@ describe('FormatTotalDurationPipe', () => {
             expect(formattedTotalDuration).toEqual('59 seconds');
         });
 
-        it('should return "1 minutes 0 seconds" if total duration is 60000 milliseconds', () => {
+        it('should return "1 minute" if total duration is 60000 milliseconds', () => {
             // Arrange
 
             // Act
             const formattedTotalDuration: string = formatTotalDurationPipe.transform(60000);
 
             // Assert
-            expect(formattedTotalDuration).toEqual('1 minutes 0 seconds');
+            expect(formattedTotalDuration).toEqual('1 minute');
         });
 
         it('should return "35 minutes 16 seconds" if total duration is 2116000 milliseconds', () => {
@@ -86,24 +90,74 @@ describe('FormatTotalDurationPipe', () => {
             expect(formattedTotalDuration).toEqual('35 minutes 16 seconds');
         });
 
-        it('should return "1 hours 0 minutes 0 seconds" if total duration is 3600000 milliseconds', () => {
+        it('should return "59 minutes" if total duration is 3540000 milliseconds', () => {
+            // Arrange
+
+            // Act
+            const formattedTotalDuration: string = formatTotalDurationPipe.transform(3540000);
+
+            // Assert
+            expect(formattedTotalDuration).toEqual('59 minutes');
+        });
+
+        it('should return "59 minutes 59 seconds" if total duration is 3599000 milliseconds', () => {
+            // Arrange
+
+            // Act
+            const formattedTotalDuration: string = formatTotalDurationPipe.transform(3599000);
+
+            // Assert
+            expect(formattedTotalDuration).toEqual('59 minutes 59 seconds');
+        });
+
+        it('should return "1 hour" if total duration is 3600000 milliseconds', () => {
             // Arrange
 
             // Act
             const formattedTotalDuration: string = formatTotalDurationPipe.transform(3600000);
 
             // Assert
-            expect(formattedTotalDuration).toEqual('1 hours 0 minutes 0 seconds');
+            expect(formattedTotalDuration).toEqual('1 hour');
         });
 
-        it('should return "6 hours 35 minutes 16 seconds" if total duration is 23716000 milliseconds', () => {
+        it('should return "2 hours" if total duration is 7200000 milliseconds', () => {
             // Arrange
 
             // Act
-            const formattedTotalDuration: string = formatTotalDurationPipe.transform(23716000);
+            const formattedTotalDuration: string = formatTotalDurationPipe.transform(7200000);
 
             // Assert
-            expect(formattedTotalDuration).toEqual('6 hours 35 minutes 16 seconds');
+            expect(formattedTotalDuration).toEqual('2 hours');
+        });
+
+        it('should return "7.2 hours" if total duration is 25819000 milliseconds', () => {
+            // Arrange
+
+            // Act
+            const formattedTotalDuration: string = formatTotalDurationPipe.transform(25819000);
+
+            // Assert
+            expect(formattedTotalDuration).toEqual('7.2 hours');
+        });
+
+        it('should return "1 day" if total duration is 86400000 milliseconds', () => {
+            // Arrange
+
+            // Act
+            const formattedTotalDuration: string = formatTotalDurationPipe.transform(86400000);
+
+            // Assert
+            expect(formattedTotalDuration).toEqual('1 day');
+        });
+
+        it('should return "1 day" if total duration is 86340000 milliseconds', () => {
+            // Arrange
+
+            // Act
+            const formattedTotalDuration: string = formatTotalDurationPipe.transform(86340000);
+
+            // Assert
+            expect(formattedTotalDuration).toEqual('1 day');
         });
 
         it('should return "1.5 days" if total duration is 131716000 milliseconds', () => {
--- src/app/pipes/format-total-duration.pipe.ts
+++ src/app/pipes/format-total-duration.pipe.ts
@@ -10,31 +10,47 @@ export class FormatTotalDurationPipe implements PipeTransform {
             return '';
         }
 
+        const dayText: string = this.translatorService.get('day');
         const daysText: string = this.translatorService.get('days');
+        const hourText: string = this.translatorService.get('hour');
         const hoursText: string = this.translatorService.get('hours');
+        const minuteText: string = this.translatorService.get('minute');
         const minutesText: string = this.translatorService.get('minutes');
+        const secondText: string = this.translatorService.get('second');
         const secondsText: string = this.translatorService.get('seconds');
 
-        const totalDurationInSeconds: number = Math.floor(totalDurationInMilliseconds / 1000);
+        const totalDurationInSeconds: number = totalDurationInMilliseconds / 1000;
+        const totalDurationInMinutes: number = totalDurationInSeconds / 60;
+        const totalDurationInHours: number = totalDurationInMinutes / 60;
+        const totalDurationInDays: number = totalDurationInHours / 24;
 
-        const unroundedDays: number = totalDurationInSeconds / (60 * 60 * 24);
-        const days: number = Math.floor(unroundedDays);
-        const hours: number = Math.floor(totalDurationInSeconds / (60 * 60));
-        const minutes: number = Math.floor(totalDurationInSeconds / 60) % 60;
-        const seconds: number = totalDurationInSeconds % 60;
+        if (totalDurationInDays >= 1) {
+            const days: number = Math.round(totalDurationInDays * 10) / 10;
 
-        if (days > 0) {
-            return `${unroundedDays.toFixed(1)} ${daysText}`;
+            return `${days} ${days === 1 ? dayText : daysText}`;
         }
 
-        if (hours > 0) {
-            return `${hours} ${hoursText} ${minutes} ${minutesText} ${seconds} ${secondsText}`;
+        if (totalDurationInHours >= 1) {
+            const hours: number = Math.round(totalDurationInHours * 10) / 10;
+
+            if (hours === 24) {
+                return `1 ${dayText}`;
+            }
+
+            return `${hours} ${hours === 1 ? hourText : hoursText}`;
         }
 
-        if (minutes > 0) {
-            return `${minutes} ${minutesText} ${seconds} ${secondsText}`;
+        const minutes: number = Math.floor(totalDurationInMinutes) % 60;
+        const seconds: number = Math.floor(totalDurationInSeconds) % 60;
+
+        if (totalDurationInMinutes >= 1) {
+            if (seconds > 0) {
+                return `${minutes} ${minutes === 1 ? minuteText : minutesText} ${seconds} ${seconds === 1 ? secondText : secondsText}`;
+            }
+
+            return `${minutes} ${minutes === 1 ? minuteText : minutesText}`;
         }
 
-        return `${seconds} ${secondsText}`;
+        return `${seconds} ${seconds === 1 ? secondText : secondsText}`;
     }
 }
