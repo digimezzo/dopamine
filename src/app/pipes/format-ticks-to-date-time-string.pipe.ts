8e025b4da29aaa8c2dc91537987813169b111678
Raphaël <raphael@digimezzo.com>
2022-10-20 15:06:05 +0200

    Tweaking

--- src/app/pipes/format-ticks-to-date-time-string.pipe.ts
+++ src/app/pipes/format-ticks-to-date-time-string.pipe.ts
@@ -16,6 +16,14 @@ export class FormatTicksToDateTimeStringPipe implements PipeTransform {
          // Based on https://github.com/vyushin/ticks-to-date/blob/master/src/ticksToDate.js
         const date: Date = new Date((ticks + offsetInTicks) / 10000 + new Date('0001-01-01T00:00:00Z').getTime());
 
+        // const year: number = date.getFullYear();
+        // const month: string = ('0' + (date.getMonth()+1)).slice(-2)
+        // const day: string  = ('0' + date.getDay()).slice(-2)
+        // const hours: number = date.getHours();
+        // const minutes: string = ('0' + date.getMinutes()).slice(-2)
+
+        // return `${year}-${month}-${day} ${hours}:${minutes}`;
+
         return moment(date).format('MMM DD, YYYY HH:mm');;
     }
 }
