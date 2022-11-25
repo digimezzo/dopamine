ab469a5ba96473d8caecce5bf50961d226577d12
Digimezzo <digimezzo@outlook.com>
2022-11-25 12:11:33 +0100

    Bugfix/356 error arch linux (#357)
    * Trying JS electron-builder config
    
    * All product info now comes from package.json

---
+++ electron-builder.config.js
@@ -0,0 +1,72 @@
+const { getFullVersion } = require('./get-package-information.js');
+
+const config = {
+    appId: "com.digimezzo.dopamine",
+    productName: "Dopamine",
+    fileAssociations: [
+        {
+            name: "MP3 Files",
+            description: "MP3 Files",
+            ext: "mp3",
+            icon: "build/mp3"
+        },
+        {
+            name: "FLAC Files",
+            description: "FLAC Files",
+            ext: "flac",
+            icon: "build/flac"
+        },
+        {
+            name: "OGG Files",
+            description: "OGG Files",
+            ext: "ogg",
+            icon: "build/ogg"
+        },
+        {
+            name: "M4A Files",
+            description: "M4A Files",
+            ext: "m4a",
+            icon: "build/m4a"
+        },
+        {
+            name: "OPUS Files",
+            description: "OPUS Files",
+            ext: "opus",
+            icon: "build/opus"
+        },
+        {
+            name: "WAV Files",
+            description: "WAV Files",
+            ext: "wav",
+            icon: "build/wav"
+        }
+    ],
+    nsis: {
+        shortcutName: "Dopamine 3",
+        perMachine: true
+    },
+    directories: {
+        output: "release"
+    },
+    files: ["**/*"],
+    extraResources: ["LICENSE"],
+    win: {
+        target: ["nsis"],
+        artifactName: `\${productName}-${getFullVersion()}.\${ext}`
+    },
+    mac: {
+        target: ["dmg"],
+        artifactName: `\${productName}-${getFullVersion()}.\${ext}`
+    },
+    linux: {
+        target: ["pacman"],
+        category: "Audio",
+        artifactName: `\${productName}-${getFullVersion()}.\${ext}`,
+        desktop: {
+            Name: "Dopamine 3",
+            Terminal: "false"
+        }
+    }
+};
+
+module.exports = config;
