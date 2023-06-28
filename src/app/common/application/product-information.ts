9b90d1d4d71cf4837f27c9b9705df9911b226a08
may <m4rch3n1ng@gmail.com>
2023-06-28 04:41:57 +0200

    rea-add dynamic copyright

--- src/app/common/application/product-information.ts
+++ src/app/common/application/product-information.ts
@@ -1,6 +1,6 @@
 export class ProductInformation {
     public static readonly applicationName: string = require('../../../../package.json').name;
     public static readonly applicationVersion: string = require('../../../../package.json').version;
-    public static readonly applicationCopyright: string = 'Copyright Digimezzo Ⓒ 2014 - 2022';
+    public static readonly applicationCopyright: string = require('../../../../package.json').copyright;
     public static readonly releasesDownloadUrl: string = 'https://github.com/digimezzo/dopamine/releases/';
 }
