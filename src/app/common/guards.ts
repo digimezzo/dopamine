057a0fa6b28053e509b6a81d5c975b86b5ef7cfa
Digimezzo <digimezzo@outlook.com>
2023-09-24 07:35:49 +0200

    Added Guards

--- src/app/common/guards.ts
+++ src/app/common/guards.ts
@@ -1,5 +1,5 @@
 export class Guards {
-    public static isDefined<T>(value: T | undefined | null): boolean {
-        return <T>value !== undefined && <T>value !== null;
+    public static isUndefined<T>(value: T | undefined | null): boolean {
+        return <T>value === undefined || <T>value === null;
     }
 }
