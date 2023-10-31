619d0b4d7519492e26f24941e0a01682a451f2ff
Digimezzo <digimezzo@outlook.com>
2023-10-31 06:43:36 +0100

    Adds AZLyricsAPI

---
+++ src/app/common/api/lyrics/a-z-lyrics-api.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { ILyricsApi } from './i-lyrics-api';
+import { Injectable } from '@angular/core';
+import { Lyrics } from './lyrics';
+import { HttpClient } from '@angular/common/http';
+import * as cheerio from 'cheerio';
+import { CheerioAPI } from 'cheerio';
+
+@Injectable()
+export class AZLyricsApi implements ILyricsApi {
+    public constructor(private httpClient: HttpClient) {}
+
+    public get sourceName(): string {
+        return 'AZLyrics';
+    }
+
+    public async getLyricsAsync(artist: string, title: string): Promise<Lyrics> {
+        const url: string = this.buildUrl(artist, title);
+        const response: string = await this.httpClient.get(url, { responseType: 'text' }).toPromise();
+
+        const cheerioAPI: CheerioAPI = cheerio.load(response);
+
+        // @ts-ignore
+        const lyricsDiv = cheerioAPI('.col-xs-12.col-lg-8.text-center')[0].children[14].children;
+
+        let lyrics: string = '';
+
+        for (let i: number = 2; i < lyricsDiv.length; i++) {
+            // @ts-ignore
+            const line: string = lyricsDiv[i].data != undefined ? `${lyricsDiv[i].data.substr(1)}\n` : ``;
+
+            lyrics += line;
+        }
+
+        lyrics = lyrics.slice(0, -2);
+
+        return new Lyrics(this.sourceName, lyrics);
+    }
+
+    private buildUrl(artist: string, title: string): string {
+        return `https://azlyrics.com/lyrics/${this.sanitizeForLink(artist)}/${this.sanitizeForLink(title)}.html`;
+    }
+
+    private sanitizeForLink(data: string): string {
+        data = data.replace(/\W/g, '');
+        data = data.toLowerCase();
+
+        return data;
+    }
+}
--- src/app/services/lyrics/online-lyrics-getter.ts
+++ src/app/services/lyrics/online-lyrics-getter.ts
@@ -5,13 +5,34 @@ import { ChartLyricsApi } from '../../common/api/lyrics/chart-lyrics-api';
 import { LyricsModel } from './lyrics-model';
 import { Lyrics } from '../../common/api/lyrics/lyrics';
 import { LyricsSourceType } from '../../common/api/lyrics/lyrics-source-type';
+import { AZLyricsApi } from '../../common/api/lyrics/a-z-lyrics';
+import { Logger } from '../../common/logger';
+import { Strings } from '../../common/strings';
 
 @Injectable()
 export class OnlineLyricsGetter implements ILyricsGetter {
-    public constructor(private chartLyricsApi: ChartLyricsApi) {}
+    public constructor(
+        private chartLyricsApi: ChartLyricsApi,
+        private azLyricsApi: AZLyricsApi,
+        private logger: Logger,
+    ) {}
 
     public async getLyricsAsync(track: TrackModel): Promise<LyricsModel> {
-        const lyrics: Lyrics = await this.chartLyricsApi.getLyricsAsync(track.rawFirstArtist, track.title);
+        let lyrics: Lyrics = Lyrics.default();
+
+        try {
+            lyrics = await this.chartLyricsApi.getLyricsAsync(track.rawFirstArtist, track.title);
+        } catch (e) {
+            this.logger.error(e, 'Could not get lyrics from ChartLyrics', 'OnlineLyricsGetter', 'getLyricsAsync');
+        }
+
+        if (Strings.isNullOrWhiteSpace(lyrics.text)) {
+            try {
+                lyrics = await this.azLyricsApi.getLyricsAsync(track.rawFirstArtist, track.title);
+            } catch (e) {
+                this.logger.error(e, 'Could not get lyrics from AZLyrics', 'OnlineLyricsGetter', 'getLyricsAsync');
+            }
+        }
 
         return new LyricsModel(lyrics.sourceName, LyricsSourceType.online, lyrics.text);
     }
