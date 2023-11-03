d21179a0f06eef5e6669a8716d139ec81716ada5
Digimezzo <digimezzo@outlook.com>
2023-11-03 22:12:09 +0100

    Fixes #472: Update node-taglib-sharp and phase out music-metadata

--- src/app/common/metadata/file-metadata-factory.ts
+++ src/app/common/metadata/file-metadata-factory.ts
@@ -1,44 +1,12 @@
 import { Injectable } from '@angular/core';
-import { FileFormats } from '../application/file-formats';
-import { BaseFileAccess } from '../io/base-file-access';
 import { BaseFileMetadataFactory } from './base-file-metadata-factory';
 import { IFileMetadata } from './i-file-metadata';
-import { MusicMetadataFileMetadata } from './music-metadata-file-meta-data';
 import { TagLibFileMetadata } from './tag-lib-file-metadata';
 
 @Injectable()
 export class FileMetadataFactory implements BaseFileMetadataFactory {
-    public constructor(private fileAccess: BaseFileAccess) {}
-
     public async createAsync(path: string): Promise<IFileMetadata> {
-        let fileMetadata: IFileMetadata;
-
-        const fileExtension = this.fileAccess.getFileExtension(path).toLowerCase();
-
-        switch (fileExtension) {
-            case FileFormats.mp3:
-                fileMetadata = new TagLibFileMetadata(path);
-                break;
-            case FileFormats.flac:
-                fileMetadata = new TagLibFileMetadata(path);
-                break;
-            case FileFormats.ogg:
-                fileMetadata = new TagLibFileMetadata(path);
-                break;
-            case FileFormats.m4a:
-                fileMetadata = new MusicMetadataFileMetadata(path);
-                break;
-            case FileFormats.opus:
-                fileMetadata = new MusicMetadataFileMetadata(path);
-                break;
-            case FileFormats.wav:
-                fileMetadata = new TagLibFileMetadata(path);
-                break;
-            default:
-                fileMetadata = new TagLibFileMetadata(path);
-                break;
-        }
-
+        const fileMetadata: IFileMetadata = new TagLibFileMetadata(path);
         await fileMetadata.loadAsync();
 
         return fileMetadata;
