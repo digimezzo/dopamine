8864b477304651c364243140b93a1f71297d21d5
Digimezzo <digimezzo@outlook.com>
2023-11-04 13:58:35 +0100

    Fixes Typescript error about iterables

--- src/app/services/drag-and-drop/drag-and-drop.service.ts
+++ src/app/services/drag-and-drop/drag-and-drop.service.ts
@@ -1,30 +1,25 @@
-import {Injectable} from "@angular/core";
-import {BaseDragAndDropService} from "./base-drag-and-drop.service";
+import { Injectable } from '@angular/core';
+import { BaseDragAndDropService } from './base-drag-and-drop.service';
 
 @Injectable()
-export class DragAndDropService implements BaseDragAndDropService{
+export class DragAndDropService implements BaseDragAndDropService {
     public listenToOperatingSystemFileDrops(): void {
         document.addEventListener('dragover', (e) => {
             e.preventDefault();
             e.stopPropagation();
         });
-        
+
         document.addEventListener('drop', (event) => {
             event.preventDefault();
             event.stopPropagation();
-            
-            alert('Dropped file(s)');
 
-            // let pathArr = [];
-            // for (const f of event.dataTransfer.files) {
-            //     // Using the path attribute to get absolute file path
-            //     console.log('File Path of dragged files: ', f.path)
-            //     pathArr.push(f.path); // assemble array for main.js
-            // }
-            // console.log(pathArr);
-            // const ret = ipcRenderer.sendSync('dropped-file', pathArr);
-            // console.log(ret);
+            if (event.dataTransfer == undefined) {
+                return;
+            }
+
+            for (const f of event.dataTransfer.files) {
+                alert(`Dropped file: ${f.path}`);
+            }
         });
     }
-    
 }
