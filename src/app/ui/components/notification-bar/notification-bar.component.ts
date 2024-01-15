14ffe6c46c264a7d5636effa99a09d68b03f0ef6
Digimezzo <digimezzo@outlook.com>
2024-01-15 22:29:18 +0100

    Add correction for notification bar

--- src/app/ui/components/notification-bar/notification-bar.component.ts
+++ src/app/ui/components/notification-bar/notification-bar.component.ts
@@ -2,6 +2,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component, OnInit } from '@angular/core';
 import { SnackBarServiceBase } from '../../../services/snack-bar/snack-bar.service.base';
 import { Subscription } from 'rxjs';
+import { DocumentProxy } from '../../../common/io/document-proxy';
 
 @Component({
     selector: 'app-notification-bar',
@@ -19,17 +20,24 @@ import { Subscription } from 'rxjs';
 export class NotificationBarComponent implements OnInit {
     private subscription: Subscription = new Subscription();
 
-    public constructor(private snackbarService: SnackBarServiceBase) {}
+    public constructor(
+        private snackbarService: SnackBarServiceBase,
+        private documentProxy: DocumentProxy,
+    ) {}
 
     public ngOnInit(): void {
         this.subscription.add(
             this.snackbarService.showNotification$.subscribe(() => {
+                const element: HTMLElement = this.documentProxy.getDocumentElement();
+                element.style.setProperty('--notification-bar-correction', '30px');
                 this.isExpanded = true;
             }),
         );
 
         this.subscription.add(
             this.snackbarService.dismissNotification$.subscribe(() => {
+                const element: HTMLElement = this.documentProxy.getDocumentElement();
+                element.style.setProperty('--notification-bar-correction', '0px');
                 this.isExpanded = false;
             }),
         );
