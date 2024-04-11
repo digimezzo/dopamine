eb49a34323a0ed8bbb9e096f788160083e645583
dermeck <dermeck@users.noreply.github.com>
2024-04-11 07:20:32 +0200

    fix #584 show albums and tracks when filtering for genres via search text (#596)
    * fix #584 show albums and tracks when filtering for genres via search text
    
    * refactor filter-pipes for tracks and albums

--- src/app/data/entities/album-data.ts
+++ src/app/data/entities/album-data.ts
@@ -5,6 +5,7 @@ export class AlbumData {
     public albumKey: string;
     public artworkId: string | undefined;
     public year: number | undefined;
+    public genres: string | undefined;
     public dateFileCreated: number | undefined;
     public dateAdded: number | undefined;
     public dateLastPlayed: number | undefined;
