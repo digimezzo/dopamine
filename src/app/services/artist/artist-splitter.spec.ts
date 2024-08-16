5200603c8dfe900ca4af38635cbd647fa4f78e99
Digimezzo Raphaël <digimezzo@outlook.com>
2024-08-16 20:20:39 +0200

    Refactors ArtistSplitter

--- src/app/services/artist/artist-splitter.spec.ts
+++ src/app/services/artist/artist-splitter.spec.ts
@@ -25,8 +25,6 @@ describe('ArtistSplitter', () => {
         translatorServiceMock.setup((x) => x.get('Artist.UnknownArtist')).returns(() => 'Unknown artist');
 
         settingsMock = new SettingsMock();
-        settingsMock.artistSplitSeparators = 'ft.;feat.;&';
-        settingsMock.artistSplitExceptions = 'Artist2 & Artist3';
     });
 
     function createArtistSplitter(): ArtistSplitter {
@@ -35,6 +33,10 @@ describe('ArtistSplitter', () => {
 
     describe('constructor', () => {
         it('should create', () => {
+            // Arrange
+            settingsMock.artistSplitSeparators = '';
+            settingsMock.artistSplitExceptions = '';
+
             // Act
             const splitter: ArtistSplitter = createArtistSplitter();
 
@@ -46,6 +48,9 @@ describe('ArtistSplitter', () => {
     describe('splitArtist', () => {
         it('should split on all configured separators for substrings that are not in the exception list', () => {
             // Arrange
+            settingsMock.artistSplitSeparators = 'ft.;feat.;&';
+            settingsMock.artistSplitExceptions = 'Artist2 & Artist3';
+
             const splitter: ArtistSplitter = createArtistSplitter();
 
             // Act
@@ -88,6 +93,52 @@ describe('ArtistSplitter', () => {
             expect(splitArtists[6].displayName).toEqual('Unknown artist');
             expect(splitArtists[6].sourceNames.length).toEqual(1);
             expect(splitArtists[6].sourceNames[0]).toEqual('');
+        });
+
+        it('should not split on anything when there are no separators', () => {
+            // Arrange
+            settingsMock.artistSplitSeparators = '';
+            settingsMock.artistSplitExceptions = '';
+
+            const splitter: ArtistSplitter = createArtistSplitter();
+
+            // Act
+            const splitArtists: ArtistModel[] = splitter.splitArtists(artists);
+
+            // Assert
+            expect(splitArtists.length).toEqual(8);
+
+            expect(splitArtists[0].displayName).toEqual('Artist1');
+            expect(splitArtists[0].sourceNames.length).toEqual(1);
+            expect(splitArtists[0].sourceNames[0]).toEqual('Artist1');
+
+            expect(splitArtists[1].displayName).toEqual('Artist2');
+            expect(splitArtists[1].sourceNames.length).toEqual(1);
+            expect(splitArtists[1].sourceNames[0]).toEqual('Artist2');
+
+            expect(splitArtists[2].displayName).toEqual('Artist1 ft. Artist2 feat. Artist3');
+            expect(splitArtists[2].sourceNames.length).toEqual(1);
+            expect(splitArtists[2].sourceNames[0]).toEqual('Artist1 ft. Artist2 feat. Artist3');
+
+            expect(splitArtists[3].displayName).toEqual('Artist1 ft. Artist2 & Artist3');
+            expect(splitArtists[3].sourceNames.length).toEqual(1);
+            expect(splitArtists[3].sourceNames[0]).toEqual('Artist1 ft. Artist2 & Artist3');
+
+            expect(splitArtists[4].displayName).toEqual('Artist2 ft. Artist3 & Artist4');
+            expect(splitArtists[4].sourceNames.length).toEqual(1);
+            expect(splitArtists[4].sourceNames[0]).toEqual('Artist2 ft. Artist3 & Artist4');
+
+            expect(splitArtists[5].displayName).toEqual('Artist4');
+            expect(splitArtists[5].sourceNames.length).toEqual(1);
+            expect(splitArtists[5].sourceNames[0]).toEqual('Artist4');
+
+            expect(splitArtists[6].displayName).toEqual('Artist3 & Artist5');
+            expect(splitArtists[6].sourceNames.length).toEqual(1);
+            expect(splitArtists[6].sourceNames[0]).toEqual('Artist3 & Artist5');
+
+            expect(splitArtists[7].displayName).toEqual('Unknown artist');
+            expect(splitArtists[7].sourceNames.length).toEqual(1);
+            expect(splitArtists[7].sourceNames[0]).toEqual('');
         });
     });
 });
--- src/app/services/artist/artist-splitter.ts
+++ src/app/services/artist/artist-splitter.ts
@@ -7,53 +7,58 @@ import { CollectionUtils } from '../../common/utils/collections-utils';
 
 @Injectable({ providedIn: 'root' })
 export class ArtistSplitter {
-    private separators: string[] = this.settings.artistSplitSeparators.split(';');
-    private exceptions: string[] = this.settings.artistSplitExceptions.split(';');
-
     public constructor(
         private translatorService: TranslatorServiceBase,
         private settings: SettingsBase,
     ) {}
 
     public splitArtists(artists: string[]): ArtistModel[] {
+        const separators: string[] =
+            this.settings.artistSplitSeparators.trim().length > 0 ? this.settings.artistSplitSeparators.split(';') : [];
+        const exceptions: string[] =
+            this.settings.artistSplitExceptions.trim().length > 0 ? this.settings.artistSplitExceptions.split(';') : [];
+
         const returnArtists: ArtistModel[] = [];
 
-        for (const artist of artists) {
-            const splitArtists: ArtistModel[] = this.splitArtist(artist);
-            for (const splitArtist of splitArtists) {
-                if (
-                    CollectionUtils.includesIgnoreCase(
-                        returnArtists.map((x) => x.displayName),
-                        splitArtist.displayName,
-                    )
-                ) {
-                    returnArtists
-                        .find((x) => StringUtils.equalsIgnoreCase(x.displayName, splitArtist.displayName))
-                        ?.sourceNames.push(splitArtist.sourceNames[0]);
-                } else {
-                    returnArtists.push(splitArtist);
+        if (separators.length > 0) {
+            for (const artist of artists) {
+                const splitArtists: ArtistModel[] = this.splitArtist(artist, separators, exceptions);
+                for (const splitArtist of splitArtists) {
+                    if (
+                        CollectionUtils.includesIgnoreCase(
+                            returnArtists.map((x) => x.displayName),
+                            splitArtist.displayName,
+                        )
+                    ) {
+                        returnArtists
+                            .find((x) => StringUtils.equalsIgnoreCase(x.displayName, splitArtist.displayName))
+                            ?.sourceNames.push(splitArtist.sourceNames[0]);
+                    } else {
+                        returnArtists.push(splitArtist);
+                    }
                 }
             }
+        } else {
+            returnArtists.push(...artists.map((a: string) => new ArtistModel(a, a, this.translatorService)));
         }
 
         return returnArtists;
     }
 
-    private splitArtist(artist: string): ArtistModel[] {
+    private splitArtist(artist: string, separators: string[], exceptions: string[]): ArtistModel[] {
         const originalArtist: string = artist;
         const artists: ArtistModel[] = [];
 
-        for (const exception of this.exceptions) {
+        for (const exception of exceptions) {
             if (artist.includes(exception)) {
                 artists.push(new ArtistModel(originalArtist, exception, this.translatorService));
+                artist = artist.replace(exception, '20384fb2-2042-4779-8fcf-7f24f3807314');
             }
-
-            artist = artist.replace(exception, '');
         }
 
-        const regex: RegExp = new RegExp(this.separators.join('|'), 'i');
+        const regex: RegExp = new RegExp(separators.join('|'), 'i');
         artists.push(...artist.split(regex).map((a: string) => new ArtistModel(originalArtist, a.trim(), this.translatorService)));
 
-        return artists.filter((a: ArtistModel): boolean => a.name !== '');
+        return artists.filter((a: ArtistModel): boolean => a.name !== '20384fb2-2042-4779-8fcf-7f24f3807314');
     }
 }
