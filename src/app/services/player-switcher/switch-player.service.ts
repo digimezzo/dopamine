b416f89a1fb640723ffac2909cc3b0e4448cab7e
Digimezzo Raphaël <digimezzo@outlook.com>
2024-10-20 20:03:28 +0200

    Adds even more logging

--- src/app/services/player-switcher/switch-player.service.ts
+++ src/app/services/player-switcher/switch-player.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { IpcProxyBase } from '../../common/io/ipc-proxy.base';
 import { SettingsBase } from '../../common/settings/settings.base';
 import { NavigationServiceBase } from '../navigation/navigation.service.base';
+import { Logger } from '../../common/logger';
 
 @Injectable({ providedIn: 'root' })
 export class SwitchPlayerService {
@@ -9,13 +10,17 @@ export class SwitchPlayerService {
         private navigationService: NavigationServiceBase,
         private ipcProxy: IpcProxyBase,
         private settings: SettingsBase,
+        private logger: Logger,
     ) {}
 
     public async togglePlayerAsync(): Promise<void> {
+        this.logger.info('Switching player', 'SwitchPlayerService', 'togglePlayerAsync');
         if (this.settings.playerType === 'cover') {
+            this.logger.info('Detected playerType = cover. Requesting to set full player.', 'SwitchPlayerService', 'togglePlayerAsync');
             await this.navigationService.navigateToCollectionAsync();
             this.ipcProxy.sendToMainProcess('set-full-player', undefined);
         } else {
+            this.logger.info('Detected playerType = full. Requesting to set cover player.', 'SwitchPlayerService', 'togglePlayerAsync');
             await this.navigationService.navigateToCoverPlayerAsync();
             this.ipcProxy.sendToMainProcess('set-cover-player', undefined);
         }
