b5960422af08c393fe7140f0a82f56029cf58572
Digimezzo Raphaël <digimezzo@outlook.com>
2025-06-09 07:48:35 +0200

    Tweak iterable menu

--- src/app/ui/components/common/iterable-menu.component.ts
+++ src/app/ui/components/common/iterable-menu.component.ts
@@ -3,9 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'app-iterable-menu',
     templateUrl: './iterable-menu.component.html',
-    styleUrls: ['./iterable-menu.component.css'],
+    styleUrls: ['./iterable-menu.component.scss'],
 })
 export class IterableMenuComponent<T> {
+    @Input() public highContrast: boolean = false;
     @Input() public tooltipKey!: string;
     @Input() public currentItem!: T;
     @Input() public items: T[] = [];
