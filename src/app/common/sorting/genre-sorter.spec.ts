1d9f0c21873caf3f5b9adb114fe5703872904c52
nasirov <47319902+nasirov@users.noreply.github.com>
2025-07-04 07:18:05 +0400

    Fix and refactor artists and genres sorting

--- src/app/common/sorting/genre-sorter.spec.ts
+++ src/app/common/sorting/genre-sorter.spec.ts
@@ -15,6 +15,8 @@ describe('GenreSorter', () => {
     let genreModel8: GenreModel;
     let genreModel9: GenreModel;
     let genreModel10: GenreModel;
+    let genreModel11: GenreModel;
+    let genreModel12: GenreModel;
 
     let translatorServiceMock: IMock<TranslatorServiceBase>;
     let loggerMock: IMock<Logger>;
@@ -36,6 +38,8 @@ describe('GenreSorter', () => {
         genreModel8 = new GenreModel('Genre8', translatorServiceMock.object);
         genreModel9 = new GenreModel('Genre9', translatorServiceMock.object);
         genreModel10 = new GenreModel('Genre10', translatorServiceMock.object);
+        genreModel11 = new GenreModel('', translatorServiceMock.object);
+        genreModel12 = new GenreModel('Жанр', translatorServiceMock.object);
 
         genres = [
             genreModel2,
@@ -47,6 +51,8 @@ describe('GenreSorter', () => {
             genreModel1,
             genreModel7,
             genreModel5,
+            genreModel11,
+            genreModel12,
             genreModel3,
         ];
 
@@ -81,16 +87,19 @@ describe('GenreSorter', () => {
             const sortedGenres: GenreModel[] = genreSorter.sortAscending(genres);
 
             // Assert
-            expect(sortedGenres[0]).toBe(genreModel1);
-            expect(sortedGenres[1]).toBe(genreModel2);
-            expect(sortedGenres[2]).toBe(genreModel3);
-            expect(sortedGenres[3]).toBe(genreModel4);
-            expect(sortedGenres[4]).toBe(genreModel5);
-            expect(sortedGenres[5]).toBe(genreModel6);
-            expect(sortedGenres[6]).toBe(genreModel7);
-            expect(sortedGenres[7]).toBe(genreModel8);
-            expect(sortedGenres[8]).toBe(genreModel9);
-            expect(sortedGenres[9]).toBe(genreModel10);
+            expect(sortedGenres.length).toEqual(12);
+            expect(sortedGenres[0]).toBe(genreModel11);
+            expect(sortedGenres[1]).toBe(genreModel12);
+            expect(sortedGenres[2]).toBe(genreModel1);
+            expect(sortedGenres[3]).toBe(genreModel2);
+            expect(sortedGenres[4]).toBe(genreModel3);
+            expect(sortedGenres[5]).toBe(genreModel4);
+            expect(sortedGenres[6]).toBe(genreModel5);
+            expect(sortedGenres[7]).toBe(genreModel6);
+            expect(sortedGenres[8]).toBe(genreModel7);
+            expect(sortedGenres[9]).toBe(genreModel8);
+            expect(sortedGenres[10]).toBe(genreModel9);
+            expect(sortedGenres[11]).toBe(genreModel10);
         });
     });
 
@@ -122,6 +131,7 @@ describe('GenreSorter', () => {
             const sortedGenres: GenreModel[] = genreSorter.sortDescending(genres);
 
             // Assert
+            expect(sortedGenres.length).toEqual(12);
             expect(sortedGenres[0]).toBe(genreModel10);
             expect(sortedGenres[1]).toBe(genreModel9);
             expect(sortedGenres[2]).toBe(genreModel8);
@@ -132,6 +142,8 @@ describe('GenreSorter', () => {
             expect(sortedGenres[7]).toBe(genreModel3);
             expect(sortedGenres[8]).toBe(genreModel2);
             expect(sortedGenres[9]).toBe(genreModel1);
+            expect(sortedGenres[10]).toBe(genreModel12);
+            expect(sortedGenres[11]).toBe(genreModel11);
         });
     });
 });
