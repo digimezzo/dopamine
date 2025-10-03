3cc36def26d8a72d4c9ac0e081d62e19508a63ca
Digimezzo <digimezzo@outlook.com>
2025-10-03 22:33:28 +0200

    Revert "Refactor Settings and add tests (#846)"
    This reverts commit 7c02db33

--- src/app/common/settings/setting.ts
+++
@@ -1,9 +0,0 @@
-export class Setting<T> {
-    public readonly key: string;
-    public readonly defaultValue: T;
-
-    public constructor(key: string, defaultValue: T) {
-        this.key = key;
-        this.defaultValue = defaultValue;
-    }
-}
--- src/app/common/settings/store-proxy.ts
+++
@@ -1,7 +0,0 @@
-import Store from 'electron-store';
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class StoreProxy {
-    public readonly store: Store<any> = new Store();
-}
