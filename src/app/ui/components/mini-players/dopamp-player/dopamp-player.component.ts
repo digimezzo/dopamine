f29cc4673e3880d2fe51ccfaa458cf7428b8a56e
Digimezzo <digimezzo@outlook.com>
2025-10-20 20:20:54 +0200

    Further building Dopamp

--- src/app/ui/components/mini-players/dopamp-player/dopamp-player.component.ts
+++ src/app/ui/components/mini-players/dopamp-player/dopamp-player.component.ts
@@ -1,4 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AppearanceServiceBase } from '../../../../services/appearance/appearance.service.base';
+import { AudioVisualizer } from '../../../../services/playback/audio-visualizer';
+import { DocumentProxy } from '../../../../common/io/document-proxy';
 
 @Component({
     selector: 'app-cover-player',
@@ -8,9 +11,22 @@ import { AfterViewInit, Component, OnInit, ViewEncapsulation } from '@angular/co
     encapsulation: ViewEncapsulation.None,
 })
 export class DopampPlayerComponent implements OnInit, AfterViewInit {
-    public constructor() {}
+    public constructor(
+        public appearanceService: AppearanceServiceBase,
+        private audioVisualizer: AudioVisualizer,
+        private documentProxy: DocumentProxy,
+    ) {}
 
-    public ngAfterViewInit(): void {}
+    public ngAfterViewInit(): void {
+        this.setAudioVisualizer();
+    }
 
     public ngOnInit(): void {}
+
+    public openPlaybackQueue(): void {}
+
+    private setAudioVisualizer(): void {
+        const canvas: HTMLCanvasElement = this.documentProxy.getCanvasById('dopampPlayerAudioVisualizer');
+        this.audioVisualizer.connectCanvas(canvas);
+    }
 }
