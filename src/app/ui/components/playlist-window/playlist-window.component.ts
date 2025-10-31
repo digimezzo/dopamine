6a1a97bc8eed30b13523e0a217e3ee70346660f5
Digimezzo <digimezzo@outlook.com>
2025-10-31 22:17:47 +0100

    Fix showing of playlist

--- src/app/ui/components/playlist-window/playlist-window.component.ts
+++ src/app/ui/components/playlist-window/playlist-window.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AppearanceServiceBase } from '../../../services/appearance/appearance.service.base';
 import { PlaybackQueueServiceFactory } from '../../../services/playback-queue/playback-queue-service.factory';
 import { IPlaybackQueueService } from '../../../services/playback-queue/i-playback-queue.service';
+import { NavigationServiceBase } from '../../../services/navigation/navigation.service.base';
 
 @Component({
     selector: 'app-playlist-window',
@@ -9,13 +10,18 @@ import { IPlaybackQueueService } from '../../../services/playback-queue/i-playba
     styleUrls: ['./playlist-window.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
-export class PlaylistWindowComponent {
+export class PlaylistWindowComponent implements AfterViewInit {
     public constructor(
         public appearanceService: AppearanceServiceBase,
+        public navigationService: NavigationServiceBase,
         playbackQueueServiceFactory: PlaybackQueueServiceFactory,
     ) {
         this.playbackQueueService = playbackQueueServiceFactory.createRemote();
     }
 
     public playbackQueueService: IPlaybackQueueService;
+
+    public ngAfterViewInit(): void {
+        this.navigationService.refreshPlaybackQueueList();
+    }
 }
