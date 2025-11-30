d8deeed1c07af6208eeffa088e4f91b0db82d664
Digimezzo <digimezzo@outlook.com>
2025-11-30 21:46:14 +0100

    Remove web search lyrics and inverst AZLyrics and ChartLyrics

--- src/app/services/lyrics/online-lyrics-getter.spec.ts
+++ src/app/services/lyrics/online-lyrics-getter.spec.ts
@@ -8,23 +8,20 @@ import { Lyrics } from '../../common/api/lyrics/lyrics';
 import { Logger } from '../../common/logger';
 import { ChartLyricsApi } from '../../common/api/lyrics/chart-lyrics.api';
 import { AZLyricsApi } from '../../common/api/lyrics/a-z-lyrics.api';
-import { WebSearchLyricsApi } from '../../common/api/lyrics/web-search-lyrics/web-search-lyrics.api';
 
 describe('OnlineLyricsGetter', () => {
     let chartLyricsApiMock: IMock<ChartLyricsApi>;
     let azLyricsApiMock: IMock<AZLyricsApi>;
-    let webSearchLyricsApiMock: IMock<WebSearchLyricsApi>;
     let loggerMock: IMock<Logger>;
 
     beforeEach(() => {
         chartLyricsApiMock = Mock.ofType<ChartLyricsApi>();
         azLyricsApiMock = Mock.ofType<AZLyricsApi>();
-        webSearchLyricsApiMock = Mock.ofType<WebSearchLyricsApi>();
         loggerMock = Mock.ofType<Logger>();
     });
 
     function createInstance(): OnlineLyricsGetter {
-        return new OnlineLyricsGetter(chartLyricsApiMock.object, azLyricsApiMock.object, webSearchLyricsApiMock.object, loggerMock.object);
+        return new OnlineLyricsGetter(chartLyricsApiMock.object, azLyricsApiMock.object, loggerMock.object);
     }
 
     describe('constructor', () => {
@@ -50,37 +47,16 @@ describe('OnlineLyricsGetter', () => {
             // Assert
             chartLyricsApiMock.verify((x) => x.getLyricsAsync(It.isAny(), It.isAny()), Times.never());
             azLyricsApiMock.verify((x) => x.getLyricsAsync(It.isAny(), It.isAny()), Times.never());
-            webSearchLyricsApiMock.verify((x) => x.getLyricsAsync(It.isAny(), It.isAny()), Times.never());
             expect(lyricsModel.track).toEqual(track);
             expect(lyricsModel.sourceName).toEqual('');
             expect(lyricsModel.sourceType).toEqual(LyricsSourceType.none);
             expect(lyricsModel.text).toEqual('');
         });
 
-        it('should return lyrics from ChartLyrics if available', async () => {
-            // Arrange
-            const track: TrackModel = MockCreator.createTrackModel('path', 'title', 'artists');
-            const lyrics: Lyrics = new Lyrics('ChartLyrics source', 'ChartLyrics text');
-            chartLyricsApiMock.setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle)).returns(() => Promise.resolve(lyrics));
-            const instance: OnlineLyricsGetter = createInstance();
-
-            // Act
-            const lyricsModel: LyricsModel = await instance.getLyricsAsync(track);
-
-            // Assert
-            expect(lyricsModel.track).toEqual(track);
-            expect(lyricsModel.sourceName).toEqual('ChartLyrics source');
-            expect(lyricsModel.sourceType).toEqual(LyricsSourceType.online);
-            expect(lyricsModel.text).toEqual('ChartLyrics text');
-        });
-
-        it('should return lyrics from AZLyrics if no lyrics from ChartLyrics are available', async () => {
+        it('should return lyrics from AZLyrics if available', async () => {
             // Arrange
             const track: TrackModel = MockCreator.createTrackModel('path', 'title', 'artists');
             const lyrics: Lyrics = new Lyrics('AZLyrics source', 'AZLyrics text');
-            chartLyricsApiMock
-                .setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle))
-                .returns(() => Promise.resolve(Lyrics.empty()));
             azLyricsApiMock.setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle)).returns(() => Promise.resolve(lyrics));
             const instance: OnlineLyricsGetter = createInstance();
 
@@ -94,19 +70,14 @@ describe('OnlineLyricsGetter', () => {
             expect(lyricsModel.text).toEqual('AZLyrics text');
         });
 
-        it('should return lyrics from WebSearchLyrics if no lyrics from ChartLyrics and AZLyrics are available', async () => {
+        it('should return lyrics from ChartLyrics if no lyrics from AZLyrics are available', async () => {
             // Arrange
             const track: TrackModel = MockCreator.createTrackModel('path', 'title', 'artists');
-            const lyrics: Lyrics = new Lyrics('WebSearchLyrics source', 'WebSearchLyrics text');
-            chartLyricsApiMock
-                .setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle))
-                .returns(() => Promise.resolve(Lyrics.empty()));
+            const lyrics: Lyrics = new Lyrics('ChartLyrics source', 'ChartLyrics text');
             azLyricsApiMock
                 .setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle))
                 .returns(() => Promise.resolve(Lyrics.empty()));
-            webSearchLyricsApiMock
-                .setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle))
-                .returns(() => Promise.resolve(lyrics));
+            chartLyricsApiMock.setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle)).returns(() => Promise.resolve(lyrics));
             const instance: OnlineLyricsGetter = createInstance();
 
             // Act
@@ -114,9 +85,9 @@ describe('OnlineLyricsGetter', () => {
 
             // Assert
             expect(lyricsModel.track).toEqual(track);
-            expect(lyricsModel.sourceName).toEqual('WebSearchLyrics source');
+            expect(lyricsModel.sourceName).toEqual('ChartLyrics source');
             expect(lyricsModel.sourceType).toEqual(LyricsSourceType.online);
-            expect(lyricsModel.text).toEqual('WebSearchLyrics text');
+            expect(lyricsModel.text).toEqual('ChartLyrics text');
         });
 
         it('should return empty lyrics if no online lyrics are availalble', async () => {
@@ -126,9 +97,6 @@ describe('OnlineLyricsGetter', () => {
                 .setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle))
                 .returns(() => Promise.resolve(Lyrics.empty()));
             azLyricsApiMock
-                .setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle))
-                .returns(() => Promise.resolve(Lyrics.empty()));
-            webSearchLyricsApiMock
                 .setup((x) => x.getLyricsAsync(track.rawFirstArtist, track.rawTitle))
                 .returns(() => Promise.resolve(Lyrics.empty()));
             const instance: OnlineLyricsGetter = createInstance();
--- src/app/testing/integration-test-runner.ts
+++ src/app/testing/integration-test-runner.ts
@@ -2,25 +2,17 @@ import { Injectable } from '@angular/core';
 import { AZLyricsApi } from '../common/api/lyrics/a-z-lyrics.api';
 import { Lyrics } from '../common/api/lyrics/lyrics';
 import { ChartLyricsApi } from '../common/api/lyrics/chart-lyrics.api';
-import { WebSearchLyricsApi } from '../common/api/lyrics/web-search-lyrics/web-search-lyrics.api';
 
 @Injectable()
 export class IntegrationTestRunner {
     public constructor(
         private azLyricsApi: AZLyricsApi,
         private chartLyricsApi: ChartLyricsApi,
-        private webSearchLyricsApi: WebSearchLyricsApi,
     ) {}
 
     public async executeTestsAsync(): Promise<void> {
         await this.getLyricsFromAZLyricsTestAsync();
         await this.getLyricsFromChartLyricsTestAsync();
-        await this.getLyricsFromWebSearchLyricsTestAsync();
-    }
-
-    private async getLyricsFromChartLyricsTestAsync(): Promise<void> {
-        const lyrics: Lyrics = await this.chartLyricsApi.getLyricsAsync('Massive Attack', 'Teardrop');
-        this.assertIsTrue('getLyricsFromChartLyricsTestAsync', lyrics.text.startsWith('Love, love is a verb'));
     }
 
     private async getLyricsFromAZLyricsTestAsync(): Promise<void> {
@@ -28,9 +20,9 @@ export class IntegrationTestRunner {
         this.assertIsTrue('getLyricsFromAZLyricsTestAsync', lyrics.text.startsWith('Love, love is a verb'));
     }
 
-    private async getLyricsFromWebSearchLyricsTestAsync(): Promise<void> {
-        const lyrics: Lyrics = await this.webSearchLyricsApi.getLyricsAsync('Massive Attack', 'Teardrop');
-        this.assertIsTrue('getLyricsFromWebSearchLyricsTestAsync', lyrics.text.startsWith('Love, love is a verb'));
+    private async getLyricsFromChartLyricsTestAsync(): Promise<void> {
+        const lyrics: Lyrics = await this.chartLyricsApi.getLyricsAsync('Massive Attack', 'Teardrop');
+        this.assertIsTrue('getLyricsFromChartLyricsTestAsync', lyrics.text.startsWith('Love, love is a verb'));
     }
 
     private assertIsTrue(testName: string, condition: boolean): void {
