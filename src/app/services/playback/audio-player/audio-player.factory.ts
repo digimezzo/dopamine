17ff8da41ea256c8cd412676c1fd718b3ae16dd9
Digimezzo <digimezzo@outlook.com>
2026-02-22 08:40:52 +0100

    fix: update crossfade audio player to use settings for duration

--- src/app/services/playback/audio-player/audio-player.factory.ts
+++ src/app/services/playback/audio-player/audio-player.factory.ts
@@ -18,7 +18,7 @@ export class AudioPlayerFactory {
     public create(): IAudioPlayer {
         if (this.settings.useCrossfade) {
             this.logger.info('Creating CrossfadeAudioPlayer for audio playback.', 'AudioPlayerFactory', 'create');
-            return new CrossfadeAudioPlayer(this.mathExtensions, this.logger, this.settings.crossfadeDuration);
+            return new CrossfadeAudioPlayer(this.mathExtensions, this.settings, this.logger);
         } else if (this.settings.useGaplessPlayback) {
             this.logger.info('Creating GaplessAudioPlayer for audio playback.', 'AudioPlayerFactory', 'create');
             return new GaplessAudioPlayer(this.mathExtensions, this.logger);
