c2db64bcc06a4229c5ad22fb6ffa362b8bfc6ded
Digimezzo <digimezzo@outlook.com>
2026-05-29 04:30:33 +0200

    Building the dialog

--- src/app/ui/components/dialogs/edit-album-dialog/edit-album-dialog.component.ts
+++ src/app/ui/components/dialogs/edit-album-dialog/edit-album-dialog.component.ts
@@ -8,5 +8,16 @@ import { AlbumModel } from '../../../../services/album/album-model';
     styleUrls: ['./edit-album-dialog.component.scss'],
 })
 export class EditAlbumDialogComponent {
+    public imagePath: string = '';
+    public updateFileCovers: boolean = false;
+
     public constructor(@Inject(MAT_DIALOG_DATA) public data: AlbumModel[]) {}
+
+    public onExport(): void {}
+
+    public onChange(): void {}
+
+    public onDownload(): void {}
+
+    public onRemove(): void {}
 }
