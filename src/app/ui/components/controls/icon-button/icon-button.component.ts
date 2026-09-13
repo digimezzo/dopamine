1679fb2c4516579b543b22ef1911ecdf76e2bad8
Digimezzo <digimezzo@outlook.com>
2026-09-13 16:23:24 +0200

    Fix #1229: Icons not centered in Dopamine 3.0.10

--- src/app/ui/components/controls/icon-button/icon-button.component.ts
+++ src/app/ui/components/controls/icon-button/icon-button.component.ts
@@ -6,5 +6,20 @@ import { Component, Input } from '@angular/core';
     styleUrls: ['./icon-button.component.scss'],
 })
 export class IconButtonComponent {
-    @Input() public icon: string;
+    public iconUrl: string | undefined;
+
+    @Input() public set icon(value: string) {
+        const iconName: string | undefined = value
+            ?.split(' ')
+            .find((className: string) => className.startsWith('la-'))
+            ?.substring(3);
+
+        if (iconName == undefined) {
+            this.iconUrl = undefined;
+            return;
+        }
+
+        const iconStyleSuffix: string = value.split(' ').includes('las') ? '-solid' : '';
+        this.iconUrl = `url('./assets/line-awesome/svg/${iconName}${iconStyleSuffix}.svg')`;
+    }
 }
